f6dde10ff18b3aa11bc94fdd2153588f0015acbb
B00144945 <B00144945@mytudublin.ie>
2023-10-18 10:08:14 +0100

    changed booking html

--- createNewBooking.php
+++ createNewBooking.php
@@ -1,23 +1,22 @@
-<?php 
+<?php
 
-$mid = $_POST['memberID']; 
-$cid = $_POST['courtID']; 
-$bd = $_POST['bookingDate']; 
-$st = $_POST['startTime']; 
-$et = $_POST['endTime']; 
+$mid = $_POST['memberID'];
+$cid = $_POST['courtID'];
+$bd = $_POST['bookingDate'];
+$st = $_POST['startTime'];
+$et = $_POST['endTime'];
 
 include("dbcon.php");
 
-$sql = "insert into booking(memberid,courtid,bookingDate,starttime,endtime) values ('$mid','$cid','$bd','$st','$et')";
+$sql = "insert into booking (memberid, courtid, bookingDate, starttime, endtime) VALUES ('$mid', '$cid', '$bd', '$st', '$et')";
 
 echo $sql;
-/*
+
 if (mysqli_query($conn, $sql)) {
   echo "<br>New record created successfully";
 } else {
   echo "Error: " . $sql . "<br>" . mysqli_error($conn);
 }
-*/
 
-mysqli_close($conn); 
+mysqli_close($conn);
 ?>
